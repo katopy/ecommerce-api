81105f3a65e8630fe6f8fa84290ebef62a49c108
kath <84346755+Katopy@users.noreply.github.com>
2024-12-20 09:01:34 -0600

    Setting Spring project

---
+++ src/test/java/com/nerdery/ecommerce/NerderyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdery.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerderyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
