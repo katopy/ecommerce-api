48daf7da0b3ee24cf6560605f444ba7d07c3e307
kath <84346755+Katopy@users.noreply.github.com>
2024-12-20 09:03:02 -0600

    Setting main class

---
+++ src/main/java/com/nerdery/ecommerce/NerderyApiApplication.java
@@ -0,0 +1,13 @@
+package com.nerdery.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerderyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerderyApiApplication.class, args);
+	}
+
+}
