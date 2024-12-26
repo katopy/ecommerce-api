6e1b9470871d59a9d383ae732ea0912d3f294879
kath <84346755+Katopy@users.noreply.github.com>
2024-12-26 11:49:20 -0600

    moving filterchain to a new class different from webconfig

--- src/main/java/com/nerdery/ecommerce/config/security/SecurityConfig.java
+++ src/main/java/com/nerdery/ecommerce/config/security/SecurityConfig.java
@@ -1,13 +1,48 @@
 package com.nerdery.ecommerce.config.security;
 
+import com.nerdery.ecommerce.config.security.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    private AuthenticationProvider daoAuthProvider;
+    private final AuthenticationProvider daoAuthProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(ses-> ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(daoAuthProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(SecurityConfig::buildRequestMatchers)
+                .build();
+    }
+
+    private static void buildRequestMatchers(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authReqConfig) {
+        authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.POST, "/auth/login").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate").permitAll();
+
+
+
+        authReqConfig.anyRequest().authenticated();
+    }
+
+
 }
--- src/main/java/com/nerdery/ecommerce/config/security/WebConfiguration.java
+++ src/main/java/com/nerdery/ecommerce/config/security/WebConfiguration.java
@@ -17,22 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableSpringDataWebSupport(
         pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfiguration {
-    private final AuthenticationProvider daoAuthProvider;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(ses-> ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(daoAuthProvider)
-                .authorizeHttpRequests(WebConfiguration::buildRequestMatchers)
-                .build();
-    }
 
-    private static void buildRequestMatchers(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authReqConfig) {
-        authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
-        authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
-
-        authReqConfig.anyRequest().authenticated();
-    }
 }
