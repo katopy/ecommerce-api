bc4f502c54c8ce069dc4f94b493ab8370d0019e7
kath <84346755+Katopy@users.noreply.github.com>
2024-12-28 15:42:48 -0600

    adding custom exceptions

--- src/main/java/com/nerdery/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nerdery/ecommerce/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.nerdery.ecommerce.exception;
 
-import com.nerdery.ecommerce.dto.ApiError;
+import com.nerdery.ecommerce.dto.responses.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -11,29 +11,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<?> handlerGenericException(Exception ex, HttpServletRequest req) {
-//        ApiError apiError = new ApiError();
-//        apiError.setBackendMessage(ex.getLocalizedMessage());
-//        apiError.setUrl(req.getRequestURL().toString());
-//        apiError.setMethod(req.getMethod());
-//        apiError.setMessage("Error from the internal server");
-//
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
-//    }
-//
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<?> handleMethodArgumentException(MethodArgumentNotValidException ex, HttpServletRequest req) {
-//        ApiError apiError = new ApiError();
-//        apiError.setBackendMessage(ex.getLocalizedMessage());
-//        apiError.setUrl(req.getRequestURL().toString());
-//        apiError.setMethod(req.getMethod());
-//        apiError.setMessage("Error with request");
-//
-//        System.out.println(ex.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage));
-//
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
-//    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handlerGenericException(Exception ex, HttpServletRequest req) {
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(ex.getLocalizedMessage());
+        apiError.setUrl(req.getRequestURL().toString());
+        apiError.setMethod(req.getMethod());
+        apiError.setMessage("Error from the internal server.");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentException(MethodArgumentNotValidException ex, HttpServletRequest req) {
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(ex.getLocalizedMessage());
+        apiError.setUrl(req.getRequestURL().toString());
+        apiError.setMethod(req.getMethod());
+        apiError.setMessage("Error with request value.");
+        System.out.println(ex.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
 
 }
--- src/main/java/com/nerdery/ecommerce/exception/ObjectNotFoundException.java
+++ src/main/java/com/nerdery/ecommerce/exception/ObjectNotFoundException.java
@@ -1,8 +1,5 @@
 package com.nerdery.ecommerce.exception;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public class ObjectNotFoundException extends RuntimeException{
     public ObjectNotFoundException(String message){
         super(message);
