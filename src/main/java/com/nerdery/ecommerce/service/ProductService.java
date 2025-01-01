412ced823beaec45e543968d9ccc9d5e3c97dcb3
kath <84346755+Katopy@users.noreply.github.com>
2024-12-31 22:50:57 -0600

    adding productimpl

--- src/main/java/com/nerdery/ecommerce/service/ProductService.java
+++ src/main/java/com/nerdery/ecommerce/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.nerdery.ecommerce.service;
 
-import com.nerdery.ecommerce.dto.SaveProduct;
+import com.nerdery.ecommerce.dto.products.ProductResponse;
+import com.nerdery.ecommerce.dto.products.SaveProduct;
 import com.nerdery.ecommerce.persistence.entity.Product;
 import org.springframework.data.domain.Page;
 
@@ -10,9 +11,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
-    Page<Product> findAll(Pageable pageable);
+    Page<ProductResponse> findAll(Pageable pageable);
 
-    Optional<Product> findById(Long id);
+    Optional<ProductResponse> findById(Long id);
 
     List<Product> getByCategory(Integer id);
 
--- src/main/java/com/nerdery/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/nerdery/ecommerce/service/impl/ProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nerdery.ecommerce.service.impl;
 
-import com.nerdery.ecommerce.dto.SaveProduct;
+import com.nerdery.ecommerce.dto.category.CategoryResponse;
+import com.nerdery.ecommerce.dto.products.ProductResponse;
+import com.nerdery.ecommerce.dto.products.SaveProduct;
 import com.nerdery.ecommerce.exception.ObjectNotFoundException;
 import com.nerdery.ecommerce.persistence.entity.Category;
 import com.nerdery.ecommerce.persistence.entity.Product;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,59 +22,75 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
     @Autowired
-    public ProductServiceImpl(ProductRepository productRepository){
+    public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
-    public Page<Product> findAll(Pageable pageable){
-        return productRepository.findAll(pageable);
+    public Page<ProductResponse> findAll(Pageable pageable) {
+        Page<Product> productsPage = productRepository.findAll(pageable);
+        return productsPage.map(product -> new ProductResponse(
+                product.getProductId(),
+                product.getName(),
+                product.getPrice(),
+                product.getDetails(),
+                new CategoryResponse(product.getCategory().getCategoryId(), product.getCategory().getName())
+        ));
     }
 
     @Override
-    public Optional<Product> findById(Long id){
-        return productRepository.findById(id);
+    public Optional<ProductResponse> findById(Long id) {
+        Optional<Product> productsPage = productRepository.findById(id);
+        return productsPage.map(product -> new ProductResponse(
+                product.getProductId(),
+                product.getName(),
+                product.getPrice(),
+                product.getDetails(),
+                new CategoryResponse(product.getCategory().getCategoryId(), product.getCategory().getName())
+        ));
     }
 
     @Override
-    public List<Product> getByCategory(Integer id){
+    public List<Product> getByCategory(Integer id) {
         return productRepository.findByCategoryId(id);
     }
 
     @Override
     public void deleteProduct(Long id) {
-         productRepository.deleteById(id);
     }
 
     @Override
-    public Product disableOneById(Long productId){
+    public Product disableOneById(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ObjectNotFoundException("Product not found with id: " + productId));
         product.setStatus(Product.ProductStatus.DISABLED);
 
         return productRepository.save(product);
-
     }
+
     @Override
-    public Product createOneProduct(SaveProduct newProduct){
+    public Product createOneProduct(SaveProduct newProduct) {
         Product product = new Product();
-        product.setName(newProduct.getName());
-        product.setPrice(newProduct.getPrice());
+        product.setName(newProduct.name());
+        product.setPrice(newProduct.price());
         product.setStatus(Product.ProductStatus.ENABLED);
+        product.setDetails(newProduct.details());
         Category category = new Category();
-        category.setCategoryId(newProduct.getCategoryId());
+        category.setCategoryId(newProduct.categoryId());
         product.setCategory(category);
 
         return productRepository.save(product);
     }
+
     @Override
-    public Product updateOneById(Long productId, SaveProduct newProduct){
+    public Product updateOneById(Long productId, SaveProduct newProduct) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ObjectNotFoundException("Product not found with id: " + productId));
-        product.setName(newProduct.getName());
-        product.setPrice(newProduct.getPrice());
+        product.setName(newProduct.name());
+        product.setPrice(newProduct.price());
+        product.setDetails(newProduct.details());
         Category category = new Category();
-        category.setCategoryId(newProduct.getCategoryId());
+        category.setCategoryId(newProduct.categoryId());
         product.setCategory(category);
 
         return productRepository.save(product);
